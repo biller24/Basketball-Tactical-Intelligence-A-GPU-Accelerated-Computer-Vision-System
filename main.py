0456aff36df88dfdc28663681e3618ff22f239cc
biller24 <120118709+biller24@users.noreply.github.com>
2026-02-12 19:57:48 +0200

    Added Zero-Shot Classification for team assignment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from utils import read_video,save_video
 from trackers import PlayerTracker,BallTracker
 from drawers import PlayerTracksDrawer
 from drawers import BallTracksDrawer
-
+from team_assigner import TeamAssigner
 def main():
 
     # Read Video
@@ -14,7 +14,16 @@ def main():
 
     # Run Trackers
     player_tracks = player_tracker.get_object_tracks(video_frames,read_from_stub= True, stub_path="stubs/player_track_stubs.pkl")
-    balL_tracks = ball_tracker.get_object_tracks(video_frames,read_from_stub= True, stub_path="stubs/ball_track_stubs.pkl")
+    ball_tracks = ball_tracker.get_object_tracks(video_frames,read_from_stub= True, stub_path="stubs/ball_track_stubs.pkl")
+
+    # Remove wrong ball Detections
+    ball_tracks = ball_tracker.remove_wrong_detections(ball_tracks)
+    # Interpolate Ball Tracks
+    ball_tracks = ball_tracker.interpolate_ball_positions(ball_tracks)
+
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    player_assignment = team_assigner.get_player_teams_across_frames(video_frames,player_tracks,read_from_stub= True, stub_path="stubs/player_team_stubs.pkl")
 
     # Draw Output
     # Initialize Drawers
@@ -22,8 +31,8 @@ def main():
     ball_tracks_drawer = BallTracksDrawer()
 
     # Draw Object Tracks
-    output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks)
-    output_video_frames = ball_tracks_drawer.draw(output_video_frames,balL_tracks)
+    output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks, player_assignment)
+    output_video_frames = ball_tracks_drawer.draw(output_video_frames,ball_tracks)
 
     # Save Video
     save_video(output_video_frames, "output_videos/output_video.avi")
